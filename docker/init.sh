44c346a310ec0b6c3be54e81f53b75d28fa6e276
Amit Singh <61005937+amit0535@users.noreply.github.com>
2024-04-28 01:25:11 +0530

    Update init.sh

--- docker/init.sh
+++ docker/init.sh
@@ -1,7 +1,7 @@
 sudo apt update
 echo 'updating...'
 sudo apt upgrade -y
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 apt-cache policy docker-ce
