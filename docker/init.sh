630b4b3586905ce28fc54a073e12ebc6b5c0b032
Amit Singh <61005937+amit0535@users.noreply.github.com>
2024-07-13 15:19:18 +0530

    Update init.sh to dynamically pick arch

--- docker/init.sh
+++ docker/init.sh
@@ -3,7 +3,7 @@ echo 'updating...'
 sudo apt upgrade -y
 sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo add-apt-repository "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/ubuntu focal stable"
 apt-cache policy docker-ce
 echo 'installing Docker...'
 sudo apt install docker-ce -y
