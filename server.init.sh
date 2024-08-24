a93265428b224b2a31326110cfe71834fa22fafa
Amit Singh <61005937+amit0535@users.noreply.github.com>
2024-08-24 22:50:20 +0530

    Update server.init.sh

--- server.init.sh
+++ server.init.sh
@@ -3,3 +3,4 @@ usermod -aG sudo amit
 ufw allow OpenSSH
 ufw enable
 #$USER ALL=(ALL) NOPASSWD: ALL
+#mkdir  /home/amit/.ssh/ &&  cp /root/.ssh/authorized_keys /home/amit/.ssh/ && chown -R amit:amit /home/amit
